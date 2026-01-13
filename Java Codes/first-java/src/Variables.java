87af8066a2069506aca04a368ae8ce0a0fc69139
Dxtro22 <suvigyabisht01@gmail.com>
2026-01-13 12:54:23 +0530

    Fix: flatten nested repository structure

---
+++ Java Codes/first-java/src/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String[] args)
+    {
+
+        //Variables
+        int a;
+        String s;
+        a=10;
+        s="Java";
+        System.out.println("a value is "+a+" "+"s value is "+s);
+
+        //Constants(they are used when the value can't be overwirtted)
+
+        final double pie = 3.14;
+        // pie = 43;this can't be done as pie is now made as constant
+
+    }
+}
